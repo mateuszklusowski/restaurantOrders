c96b8771bb3dc86fa94db12d6ebf8944a431d256
MateuszK <klusowskimat@gmail.com>
2022-05-25 19:06:51 +0200

    changes in tests

--- app/restaurant/test/test_restaurant_api.py
+++ app/restaurant/test/test_restaurant_api.py
@@ -17,14 +17,14 @@ def sample_cuisine(cuisine_name):
     return Cuisine.objects.create(name=cuisine_name)
 
 
-def detail_url(restaurant_name):
+def detail_url(restaurant_id):
     """Return restaurants detail URL"""
-    return reverse('restaurant:restaurant-detail', args=[restaurant_name])
+    return reverse('restaurant:restaurant-detail', args=[restaurant_id])
 
 
-def sample_restaurant(name):
+def sample_restaurant(restaurant_name):
     return Restaurant.objects.create(
-        name=name,
+        name=restaurant_name,
         city='Warsaw',
         country='Poland',
         address='Prosta 48',
@@ -44,8 +44,8 @@ class GetRestaurantListTest(TestCase):
 
     def test_retrieve_restaurant_list(self):
         """Test retrieving restaurant list"""
-        sample_restaurant(name='restaurant1')
-        sample_restaurant(name='restaurant2')
+        sample_restaurant('restaurant1')
+        sample_restaurant('restaurant2')
 
         res = self.client.get(RESTAURANTS_URL)
         restaurants = Restaurant.objects.all().order_by('id')
@@ -55,7 +55,7 @@ class GetRestaurantListTest(TestCase):
 
     def test_view_restaurant_detail(self):
         """Test viewing a restaurant detail"""
-        restaurant = sample_restaurant(name='restaurant2')
+        restaurant = sample_restaurant('restaurant2')
 
         url = detail_url(restaurant.id)
 
@@ -68,9 +68,9 @@ class GetRestaurantListTest(TestCase):
         cuisine1 = sample_cuisine('Indian')
         cuisine2 = sample_cuisine('Italian')
 
-        restaurant = sample_restaurant(name='restaurant1')
+        restaurant = sample_restaurant('restaurant1')
         restaurant.cuisine = cuisine1
-        restaurant2 = sample_restaurant(name='restaurant2')
+        restaurant2 = sample_restaurant('restaurant2')
         restaurant2.cuisine = cuisine2
 
         payload = {'cuisine': 'Indian'}
@@ -85,10 +85,10 @@ class GetRestaurantListTest(TestCase):
 
     def test_search_by_city(self):
         """Test restaurant filtering by cuisine"""
-        restaurant = sample_restaurant(name='restaurant1')
+        restaurant = sample_restaurant('restaurant1')
         restaurant.city = 'Poznan'
         restaurant.save()
-        restaurant2 = sample_restaurant(name='restaurant2')
+        restaurant2 = sample_restaurant('restaurant2')
 
         payload = {'city': 'Poznan'}
         res = self.client.get(RESTAURANTS_URL, payload)
