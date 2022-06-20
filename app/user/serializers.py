947e62b133dc8b19065759393908a4f49ba9d769
MateuszK <klusowskimat@gmail.com>
2022-06-20 18:52:32 +0200

    Added comments

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,5 +1,12 @@
 from django.contrib.auth import get_user_model, authenticate
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.utils.encoding import force_str, smart_bytes
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.core.mail import send_mail
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls import reverse
+from django.conf import settings
 
 from rest_framework import serializers
 
@@ -73,3 +80,82 @@ class UserPasswordUpdateSerializer(serializers.Serializer):
             msg = _('Old password is incorrect or similar to new one')
             raise serializers.ValidationError(msg, code='password')
         return data
+
+
+class PasswordResetRequestSerializer(serializers.Serializer):
+    """Serializer for password reset request"""
+    email = serializers.EmailField()
+
+    class Meta:
+        fields = '__all__'
+
+    def validate(self, attrs):
+        """Check that user exists"""
+        email = attrs.get('email')
+
+        if get_user_model().objects.filter(email=email).exists():
+            user = get_user_model().objects.get(email=email)
+            uidb64 = urlsafe_base64_encode(smart_bytes(user.pk))
+            token = PasswordResetTokenGenerator().make_token(user)
+            current_site = get_current_site(self.context.get('request')).domain
+            relative_url = reverse('user:reset-password-confirm', kwargs={'uidb64': uidb64, 'token': token})
+            absolute_url = 'http://{}{}'.format(current_site, relative_url)
+            email_message = 'Here is your password reset link:\n{}'.format(absolute_url)
+            send_mail(
+                subject='Reset your password',
+                message=email_message,
+                from_email=settings.EMAIL_HOST_USER,
+                recipient_list=[user.email]
+            )
+        else:
+            msg = _('No user with this email address exists')
+            raise serializers.ValidationError(msg, code='email')
+
+        return attrs
+
+
+class SetNewPasswordSerializer(serializers.Serializer):
+    """Serializer for setting new password"""
+    new_password1 = serializers.CharField(
+        style={'input_type': 'password'},
+        required=True
+    )
+    new_password2 = serializers.CharField(
+        style={'input_type': 'password'},
+        required=True
+    )
+
+    class Meta:
+        extra_kwargs = {
+            'new_password1': {'write_only': True, 'min_length': 5},
+            'new_password2': {'write_only': True, 'min_length': 5}
+        }
+
+    def validate(self, attrs):
+        """Validate passwords and check token availability"""
+        uidb64 = self.context.get('request').path.split('/')[-3]
+        token = self.context.get('request').path.split('/')[-2]
+        user_id = force_str(urlsafe_base64_decode(uidb64))
+        user = get_user_model().objects.get(pk=user_id)
+
+        if user:
+            if not PasswordResetTokenGenerator().check_token(user, token):
+                msg = _('Invalid token or expired')
+                raise serializers.ValidationError({'token': msg}, code='token')
+
+            if attrs.get('new_password1') != attrs.get('new_password2'):
+                msg = _('Passwords do not match')
+                raise serializers.ValidationError({'password': msg}, code='password')
+
+            if user.check_password(attrs.get('new_password1')):
+                msg = _('New password is similar to old one')
+                raise serializers.ValidationError({'password': msg}, code='password')
+
+            user.set_password(attrs.get('new_password1'))
+            user.save()
+
+            return (user)
+
+        else:
+            msg = _('Invalid token or expired')
+            raise serializers.ValidationError({'token': msg}, code='token')
