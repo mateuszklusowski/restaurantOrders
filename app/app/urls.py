87d7f6cce89a9b46d19ac263297f62245cc3520c
MateuszK <klusowskimat@gmail.com>
2022-07-13 19:29:59 +0200

    changes restaurant route

--- app/app/urls.py
+++ app/app/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/restaurant/', include('restaurant.urls')),
+    path('api/restaurants/', include('restaurant.urls')),
     path('api/user/', include('user.urls')),
     path('api/orders/', include('order.urls'))
 ]
