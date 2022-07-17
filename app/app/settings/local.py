a813709b1cbbe9cc28b644cf15d40f1f30cc3801
MateuszK <klusowskimat@gmail.com>
2022-07-17 19:40:16 +0200

    fixin workflows

--- app/app/settings/local.py
+++ app/app/settings/local.py
@@ -89,7 +89,6 @@ DATABASES = {
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
-        'PORT': '5432',
     }
 }
 
